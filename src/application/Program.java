f4043afb44d1897a31e747ee79caa706d1407c77
Lucas Carlos <lucascarlos945@gmail.com>
2024-01-22 17:05:44 -0300

    FindById()

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@
 
 import java.util.Date;
 
+import model.DAO.DAOFactory;
+import model.DAO.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,10 +11,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-		Seller obj2 = new Seller(21,"Lucas","Lucascarlos@gmail.com", new Date(), 3000.0, obj);
-		System.out.println(obj2);
+		SellerDAO sellerDao = DAOFactory.createSellerDAO();
+		
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 	}	
 
 }
